9c4fc955e2fc5103733165ae9e5d87ff38c2b25f
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-05-08 16:17:38 -0500

    added a module for heroes

---
+++ src/app/heroes/heroes.module..ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [
+    HeroComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeroComponent,
+    ListComponent
+  ]
+})
+export class HeroesModule { }
